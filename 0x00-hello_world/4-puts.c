f07e0a780785f3022fc6948b7bf56c7518044088
gichobih <dennisgichobih@gmail.com>
2023-06-15 08:47:55 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,14 +2,14 @@
 
 
 /**
- *main - prints "programing is like building a multilingual 
+ *main - prints "programing is like building a multilingual
  *puzzle, followed by new line.
  *return: always 0.
  */
 
-int main(void) 
+int main(void)
 
 {
-        puts("\"Programming is like building a multilingual puzzle");
+        puts("\"Programming is like building a multilingual puzzle"):
         return (0);
 }
