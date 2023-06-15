80a402fda47b2019c463d2652bbb819ac673cafe
gichobih <dennisgichobih@gmail.com>
2023-06-15 08:00:54 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,11 +3,11 @@
 /**
   * main - print "programing is like building a multilingual 
   * puzzle,followed by new line.
-  * return: always 0.
+  * return :always 0.
   */
 
 int main(void) 
 {
     puts("\"Programming is like building a multilingual puzzle");
-    return 0;
+    return (0);
 }
