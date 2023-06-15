828249fbbe5fc6b03de241a59df23a80d3872806
gichobih <dennisgichobih@gmail.com>
2023-06-15 09:57:55 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Description: Prints the sizes of various data types
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	printf("Size of a char: %zu byte(s)\n", sizeof(char));
@@ -8,6 +15,6 @@ int main(void)
 	printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
 	printf("Size of a float: %zu byte(s)\n", sizeof(float));
 
-	return 0;
+	return (0);
 }
 
