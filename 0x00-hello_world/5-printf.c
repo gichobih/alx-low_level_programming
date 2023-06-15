caf3843b787f5adde720835157654f9ea922428f
gichobih <dennisgichobih@gmail.com>
2023-06-15 09:25:42 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,15 +1,12 @@
-#include<stdio.h>
+#include <stdio.h>
+
 /**
  * main - Entry point
  *
  * Return: always 0 (Success)
  */
-
 int main(void)
 {
-
-    printf("with proper grammer, but the outcome is a part of art,\n");
+    printf("with proper grammar, but the outcome is a piece of art,\n");
     return (0);
-
 }
-
