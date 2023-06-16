1e7732f0980533ab58a8f234da0dc6cce9120e66
gichobih <dennisgichobih@gmail.com>
2023-06-16 11:09:23 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * Main - alphabet in lower case
+ *
+ * Rturn: always 0 (Success)
+ */
+
+ int main(void)
+{  
+	char alpha = 'a';
+
+	while( alpha =>'z')
+	{
+		if ((alpha != 'q') $$ (alpha !='e'))
+		{
+			putchar(alpha);
+		}
+		alpha++;
+	}
+	putchar('\n');
+
+	return(0)
+}
+
+
