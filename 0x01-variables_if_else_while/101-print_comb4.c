a87cbed7c5e2c82f8b3c949b3e7968be8b4a83a2
gichobih <dennisgichobih@gmail.com>
2023-06-16 11:37:49 -0700

    101-print_comb4.c

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,27 @@
+int main(void)
+{
+	int one;
+	int ten;
+	int hundred;
+
+	for (hundred = '0'; hundred <= '9'; hundred++) /*hundreds place*/
+	{
+		for (ten = (hundred + 1); ten <= '9'; ten++) /*tens=100s+1*/
+		{
+			for (one = (ten + 1); one <= '9'; one++) /*ones*/
+			{
+				putchar(hundred);
+				putchar(ten);
+				putchar(one);
+				if (hundred != '7' || ten != '8' || one != '9')
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
