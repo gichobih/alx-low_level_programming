cbb1891418cca1f86cdedf23177d054420a7c400
gichobih <dennisgichobih@gmail.com>
2023-06-16 11:25:37 -0700

    6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -7,17 +7,13 @@
  */
 int main(void)
 {
-	putchar('0');
-	putchar('1');
-	putchar('2');
-	putchar('3');
-	putchar('4');
-	putchar('5');
-	putchar('6');
-	putchar('7');
-	putchar('8');
-	putchar('9');
+	int digit;
+
+	for (digit = 0; digit <= 9; digit++)
+		putchar(digit + '0');
+
 	putchar('\n');
 
 	return (0);
 }
+
