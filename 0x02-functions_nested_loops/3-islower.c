24668464f1e9da3ce8290329d636f50f219c4f06
gichobih <dennisgichobih@gmail.com>
2023-06-20 07:53:20 -0700

    3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,19 +1,14 @@
-#include "main.h"
-
 /**
- * _islower - checks for lowercase character
- * @c: character to check
- * Return: 1 if lowercase, 0 if uppercase
+ * _islower - Checks if a character is lowercase.
+ * @c: The character to be checked.
+ *
+ * Return: 1 if c is lowercase, 0 otherwise.
  */
-
 int _islower(int c)
 {
-	if (c >= 'a' && c <= 'z')
-	{
-		return (1);
-	}
-	else
-	{
-		return (0);
-	}
-	_putchar('\n');
+    if (c >= 'a' && c <= 'z')
+        return 1;
+    else
+        return 0;
+}
+
--- 0x02-functions_nested_loops/main.c
+++ 0x02-functions_nested_loops/main.c
@@ -1,26 +1,15 @@
-#include "main.h"
+#include <stdio.h>
 
-/**
- * test_islower - Test the _islower function
- *
- * @n: Number to pass to _islower function
- */
-void test_islower(int n)
-{
-	int r;
+int _islower(int c);
 
-         test_islower(n);
-	_putchar(r + '0');
-	_putchar('\n');
-}
-
-/**
- * main - check the code for Holberton School students.
- *
- * Return: Always 0.
- */
 int main(void)
 {
-	test_islower('H');
-	return (0);
+    char ch = 'a';
+    if (_islower(ch))
+        printf("Character is lowercase.\n");
+    else
+        printf("Character is not lowercase.\n");
+
+    return 0;
 }
+
