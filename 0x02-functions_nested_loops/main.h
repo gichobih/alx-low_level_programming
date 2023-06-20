bee7f40ac1b5a1d9923d390587a6320949551931
gichobih <dennisgichobih@gmail.com>
2023-06-20 15:01:36 -0700

    4-isalpha

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -2,13 +2,13 @@
 #define MAIN_H
 
 int _putchar(char c);
-int _islower(int c);
 int print_last_digit(int n);
 void jack_bauer(void);
 void times_table(void);
 int add(int, int);
 void print_to_98(int n);
 void print_times_table(int n);
+int _isalpha(int c);
 
 #endif /* MAIN_H */
 
