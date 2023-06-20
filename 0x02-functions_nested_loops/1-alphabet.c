d1b1d7ae1b9b91c5b1f0d88700dd511cd2b6e404
gichobih <dennisgichobih@gmail.com>
2023-06-20 04:53:35 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ *  I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game.
+ *
+ *  always return (0) 0n success
+ */
+int main(void)
+{
+	char alpha;
+
+	alpha= 'a';
+
+	while (alpha <= 'z')
+	{
+		_putchar(alpha);
+                alpha++;
+        }
+
+	_putchar ('\n');
+}
+
+	
