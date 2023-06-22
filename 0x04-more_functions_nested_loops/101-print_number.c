43fa3ec9adc4def93dc6d916d295fce38b91db92
gichobih <dennisgichobih@gmail.com>
2023-06-22 10:07:00 -0700

    101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,5 +1,5 @@
-#include <stdio.h>
 #include "main.h"
+#include <stdio.h>
 
 /**
  * print_number - print an integer, without using long, arrays, or pointers
--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,23 +1,23 @@
+#include "main.h"
 #include <stdio.h>
 
 /**
- * more_numbers - Prints 10 times the numbers from 0 to 14, followed by a new line
+ * more_numbers - print 0 to 14 ten times
  */
+
 void more_numbers(void)
 {
-	int i, j;
+	char i;
+	int counter;
 
-	for (j = 0; j < 10; j++)
+	for (counter = 1; counter <= 10; counter++)
 	{
 		for (i = 0; i <= 14; i++)
 		{
-			if (i > 9)
+			if (i / 10 > 0)
 				putchar((i / 10) + '0');
-
 			putchar((i % 10) + '0');
 		}
-
 		putchar('\n');
 	}
 }
-
