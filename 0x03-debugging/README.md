fbe86a4039c270f46803f5155a348b006e3ce034
gichobih <dennisgichobih@gmail.com>
2023-06-22 02:38:06 -0700

    0-main.c

---
+++ 0x03-debugging/README.md
@@ -0,0 +1,1 @@
+learning debugging at alx
---
+++ 0x03-debugging/main.h
@@ -0,0 +1,8 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+/*print integer if is positive or negative*/
+void positive_or_negative (int i);
+
+#endif /*MAIN_H*/
