2f22653fefcff63473d6a429b6d7c846ceb6dfd8
gichobih <dennisgichobih@gmail.com>
2023-06-22 04:20:51 -0700

    0-isuper

---
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -0,0 +1,14 @@
+#include <unistd.h>
+
+/**
+ * _putchar - Writes a character to the standard output (stdout).
+ * @c: The character to be written.
+ *
+ * Return: On success, returns the number of characters written.
+ * On error, returns -1 and sets errno appropriately.
+ */
+int _putchar(char c)
+{
+    return write(1, &c, 1);
+}
+
---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,7 @@
+#ifndef MAIN_H
+#define MAIN_H
+/**check if the character is uppercase
+ * @c is the character to check*/
+int _isupper(int c);
+
+#endif /* MAIN.H */
