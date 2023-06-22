8b1da7b63128462f012a9cd71176fa111c5c6f75
gichobih <dennisgichobih@gmail.com>
2023-06-22 02:48:19 -0700

    0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,5 +1,10 @@
 #include "main.h"
 
+/**
+ * main - determing postive_or_negative
+ *
+ * Return: Always 0
+ */
 int main(void)
 {
     int i;
--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -2,7 +2,7 @@
 #define MAIN_H
 
 #include <stdio.h>
-/*print integer if is positive or negative*/
-void positive_or_negative (int i);
 
-#endif /*MAIN_H*/
+void positive_or_negative(int i);
+
+#endif /* MAIN_H */
