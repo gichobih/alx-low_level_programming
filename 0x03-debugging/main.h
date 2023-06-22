1e4ad6192e1f107569610aedce635cac3c654f1b
gichobih <dennisgichobih@gmail.com>
2023-06-22 12:12:17 -0700

    2-largest_number.c

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,10 +1,15 @@
-carrie@ubuntu:/debugging$ cat main.h
 #ifndef MAIN_H
 #define MAIN_H
 
 #include <stdio.h>
 
 void positive_or_negative(int i);
+/**
+ * largest_number - returns the largest of 3 numbers
+ * @a: first integer
+ * @b: second integer
+ * @c: third integer
+ */
+int largest_number(int a, int b, int c);
 
-#endif /* MAIN_H */
-carrie@ubuntu:/debugging$ 
+#endif /* MAIN_H */ 
