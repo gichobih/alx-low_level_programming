afd82a4a994dae001543080a443df86cd4bc0921
gichobih <dennisgichobih@gmail.com>
2023-06-22 04:27:54 -0700

    0-isuper

--- 0x04-more_functions_nested_loops/0-main.c
+++ 0x04-more_functions_nested_loops/0-main.c
@@ -11,8 +11,12 @@ int main(void)
     char c;
 
     c = 'A';
-    printf("%c: %d\n", c, _isupper(c));
+    {
+	    printf("%c: %d\n", c, _isupper(c));
+    }
     c = 'a';
-    printf("%c: %d\n", c, _isupper(c));
+    {
+	    printf("%c: %d\n", c, _isupper(c));
+    }	    
     return (0);
 }
