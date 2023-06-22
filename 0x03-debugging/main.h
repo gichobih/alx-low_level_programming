c0200d28e4c2c3dd553a22c369a7aab43d63eb63
gichobih <dennisgichobih@gmail.com>
2023-06-22 03:00:00 -0700

    0-main.c

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,8 +1,13 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stdio.h>
+#include <time.h>
 
+/**
+ * positive_or_negative - Determines if a number is positive or negative
+ * @i: The number to check
+ */
 void positive_or_negative(int i);
 
 #endif /* MAIN_H */
+
