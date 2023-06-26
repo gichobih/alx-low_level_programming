7a1deabd6768b3aa63da4c188033f8c987cef264
gichobih <dennisgichobih@gmail.com>
2023-06-26 05:37:13 -0700

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - Updates the value pointed to by the pointer to 98.
+ * @n: Pointer to an integer.
+ *
+ * Return: None.
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
+
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,9 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/* reset_to_98 - Updates the value pointed to by the pointer to 98.
+ * @n: Pointer to an integer.*/
+void reset_to_98(int *n);
+
+#endif /* MAIN_H */
+
