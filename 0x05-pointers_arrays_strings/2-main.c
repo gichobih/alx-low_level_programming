dce37aef7accc3af036acfb1401d235360a3d1f1
gichobih <dennisgichobih@gmail.com>
2023-06-26 07:43:57 -0700

    2-strlen

--- 0x05-pointers_arrays_strings/2-main.c
+++ 0x05-pointers_arrays_strings/2-main.c
@@ -1,15 +1,20 @@
 #include "main.h"
 #include <stdio.h>
 
+/**
+ * main - check the code
+ *
+ * Return: Always 0
+ */
 int main(void)
 {
-    char *str;
-    int len;
+	char *str;
+	int len;
 
-    str = "My first strlen!";
-    len = _strlen(str);
-    printf("%d\n", len);
+	str = "My first strlen!";
+	len = _strlen(str);
+	printf("%d\n", len);
 
-    return (0);
+	return (0);
 }
 
