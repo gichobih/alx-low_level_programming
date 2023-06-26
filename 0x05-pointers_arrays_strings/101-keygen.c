fe0be451662fdf0a654403b976b7ab4d9c8de9ee
gichobih <dennisgichobih@gmail.com>
2023-06-26 10:55:52 -0700

    101-keygen

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * generate_password - Generates a random password.
+ * @length: Length of the password.
+ *
+ * Return: The generated password.
+ */
+char *generate_password(int length)
+{
+    static const char charset[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    char *password = malloc((length + 1) * sizeof(char));
+
+    if (password == NULL)
+    {
+        fprintf(stderr, "Memory allocation failed\n");
+        exit(EXIT_FAILURE);
+    }
+
+    for (int i = 0; i < length; i++)
+    {
+        int index = rand() % (sizeof(charset) - 1);
+        password[i] = charset[index];
+    }
+
+    password[length] = '\0';
+
+    return password;
+}
+
+int main(void)
+{
+    int password_length = 10;
+    char *password;
+
+    srand(time(NULL));
+
+    password = generate_password(password_length);
+
+    printf("Generated Password: %s\n", password);
+
+    free(password);
+
+    return 0;
+}
+
