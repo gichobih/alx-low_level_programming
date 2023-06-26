159ca3c9c466e04897caac7c6415a28fb0779ce9
gichobih <dennisgichobih@gmail.com>
2023-06-26 10:50:45 -0700

    100-atoi

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -35,5 +35,9 @@ void print_array(int *a, int n);
 /*char *_strcpy(char *dest, char *src); copies the string pointed to by src, including the terminating null byte (\0), to the buffer pointed to by dest.
 Return value: the pointer to dest*/
 char *_strcpy(char *dest, char *src);
+
+/*int _atoi(char *s); converts a string into an integer*/
+int _atoi(char *s);
+
 #endif /* MAIN_H */
 
