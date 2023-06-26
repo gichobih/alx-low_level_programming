24f8004afa8530d4d14420f09ae07f1c910451d5
gichobih <dennisgichobih@gmail.com>
2023-06-26 09:22:20 -0700

    6-puts2

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -11,8 +11,13 @@ void puts2(char *str)
 	int i;
 
 	for (i = 0; str[i] != '\0'; i += 2)
+	{
 		putchar(str[i]);
 
-	putchar('\n');
+		if (str[i + 1] == '\0')
+			putchar('\n');
+		else
+			putchar(' ');
+	}
 }
 
