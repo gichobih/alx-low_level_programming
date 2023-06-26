7899efdf13f2479cacf217b3b4764fcf04377ba7
gichobih <dennisgichobih@gmail.com>
2023-06-26 09:33:40 -0700

    6-puts2

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,20 +1,36 @@
 #include "main.h"
 #include <stdio.h>
+
 /**
- * puts2 - Prints every other character of a string followed by a new line.
- * @str: Pointer to the string to be printed.
- *
- * Return: None.
+ * _strlen - returns the length of a string
+ * @s: string
+ * Return: length
  */
+
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*s != '\0')
+	{
+		len++;
+		s++;
+	}
+
+	return (len);
+}
+
+/**
+ * puts2 - prints every second character of a string, followed by a new line
+ * @str: string to print
+ */
+
 void puts2(char *str)
 {
 	int i;
 
-	for (i = 0; str[i] != '\0'; i += 2)
-	{
+	for (i = 0; str[i] != '\0' && i < _strlen(str); i += 2)
 		putchar(str[i]);
-	}
 
 	putchar('\n');
 }
-
