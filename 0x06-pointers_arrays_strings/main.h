b11a8c3cee062978e64e5a355c2ff9319417f3bb
gichobih <dennisgichobih@gmail.com>
2023-06-29 03:20:39 -0700

    1-strncat

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -7,4 +7,10 @@
  * @src: The source string.*/
 char *_strcat(char *dest, char *src);
 
+/* _strncat - concatenate n bytes to destination string
+ * @dest: string to be appended to
+ * @src: string to append
+ * @n: append n number of bytes(chars)*/
+char *_strncat(char *dest, char *src, int n);
+
 #endif /* MAIN_H*/
