997fff1d0b8b6768637e7de9c370f0ebc6055f11
gichobih <dennisgichobih@gmail.com>
2023-07-03 03:56:50 -0700

    2-strchr

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -6,15 +6,17 @@
  *@n; number of bytes to fill*/
 char *_memset(char *s, char b, unsigned int n);
 
-/*/**
+/*
  * _memcpy - copy n bytes of memory from source to destination
  * @dest: destination
  * @src: source
  * @n: number of bytes*/
 char *_memcpy(char *dest, char *src, unsigned int n);
 
-
-
+/* -strchar- locate first char occurence in a string and return the pointer there
+ * @s; string to search
+ * @c; target char */
+char *_strchr(char *s, char c);
 
 
 #endif /*MAIN_H*/
