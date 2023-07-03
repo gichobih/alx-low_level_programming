9074db297c5c30dd164d8b0959329d1ce592ea2d
gichobih <dennisgichobih@gmail.com>
2023-07-03 03:27:32 -0700

    0-memset

---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+/**_memset-fills n bytes of memory with constant values.
+ *@s; pointer to memory area
+ *@b; constant value
+ *@n; number of bytes to fill*/
+char *_memset(char *s, char b, unsigned int n);
+
+
+
+
+
+#endif /*MAIN_H*/
