9ae27404d95b65a2e43c9d94f51e3ce2cf414059
gichobih <dennisgichobih@gmail.com>
2023-07-19 07:54:58 -0700

    3-main.c, 3-op_functions.c, 3-get_op_func.c, 3-calc.h

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -2,38 +2,38 @@
 #include <stdlib.h>
 #include "3-calc.h"
 
-int (*get_op_func(char *s))(int, int); /* Add the correct function prototype*/
+int (*get_op_func(char *s))(int, int); /* Add the correct function prototype */
 
 int main(int argc, char *argv[])
 {
-    int num1, num2, result;
-    int (*func_ptr)(int, int);
+	int num1, num2, result;
+	int (*func_ptr)(int, int);
 
-    if (argc != 4)
-    {
-        printf("Error\n");
-        return (98);
-    }
+	if (argc != 4)
+	{
+		printf("Error\n");
+		return (98);
+	}
 
-    num1 = atoi(argv[1]);
-    num2 = atoi(argv[3]);
-    func_ptr = get_op_func(argv[2]);
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+	func_ptr = get_op_func(argv[2]);
 
-    if (func_ptr == NULL)
-    {
-        printf("Error\n");
-        return (99);
-    }
+	if (func_ptr == NULL)
+	{
+		printf("Error\n");
+		return (99);
+	}
 
-    if ((*argv[2] == '/' || *argv[2] == '%') && num2 == 0)
-    {
-        printf("Error\n");
-        return (100);
-    }
+	if ((*argv[2] == '/' || *argv[2] == '%') && num2 == 0)
+	{
+		printf("Error\n");
+		return (100);
+	}
 
-    result = func_ptr(num1, num2);
-    printf("%d\n", result);
+	result = func_ptr(num1, num2);
+	printf("%d\n", result);
 
-    return (0);
+	return (0);
 }
 
