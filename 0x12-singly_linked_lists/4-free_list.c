012f5681d02aaad78c7ca8ea182ced5012ebf995
gichobih <dennisgichobih@gmail.com>
2023-07-27 12:27:06 -0700

    4-free_list.c

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+/**
+ * free_list - frees a list_t list.
+ * @head: pointer to list.
+ */
+void free_list(list_t *head)
+{
+	if (head)
+	{
+		free_list(head->next);
+		if (head->str)
+			free(head->str);
+		free(head);
+	}
+}
